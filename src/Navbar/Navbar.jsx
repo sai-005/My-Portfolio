2a9e175a07632db483c420291a4e40e85966456c
saipranav <140630243+sai-005@users.noreply.github.com>
2025-04-10 13:20:22 +0530

    Add active class

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useEffect,useRef} from 'react'
 import { useState } from 'react'
 import './Navbar.css'
 import logo from '../assets/logo.png'
-import {Link} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 import menu_open from "../assets/menu-open.png"
 import menu_close from "../assets/menu-close.png"
 import gsap from 'gsap';
+import up_arrow from '../assets/up-arrow.png'
 
 const Navbar = () => {
+  const navigate=useNavigate()
   // const textRefi=useRef(null);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -24,19 +26,21 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <img  className="logo" src={logo} alt="logo"/>
+      <img onClick={()=>{navigate('/')}} className="logo" src={logo} alt="logo"/>
 
       <img className='menu_open' onClick={toggleMenu} src={menu_open} alt="" />
      
       <img  className="menu_close" onClick={toggleMenu}  src={menu_close} alt="" />
      
       <ul className={`nav-menu ${isOpen ? "show" : ""}`} >
-        <Link className='link_nav' to='/' onClick={() => setIsOpen(false)}><li>Home</li></Link>
-        <Link className='link_nav' to='/about-me' onClick={() => setIsOpen(false)}> <li>About me</li></Link>
-        <Link className='link_nav' to='/project' onClick={() => setIsOpen(false)}><li>Projects</li></Link>
-        <Link className='link_nav' to='/resume' onClick={() => setIsOpen(false)}><li>Resume</li></Link>
-        <Link  className='link_nav'to='/contact' onClick={() => setIsOpen(false)}> <button>Contact</button></Link>   
+        <NavLink activeClassName="active" className='link_nav' to='/' onClick={() => setIsOpen(false)}><li>Home</li> </NavLink>
+        <NavLink activeClassName="active" className='link_nav' to='/about-me' onClick={() => setIsOpen(false)}> <li>About me</li> </NavLink>
+        <NavLink activeClassName="active" className='link_nav' to='/project' onClick={() => setIsOpen(false)}><li>Projects</li> </NavLink>
+        <NavLink activeClassName="active" className='link_nav' to='/resume' onClick={() => setIsOpen(false)}><li>Resume</li></NavLink>
+        <NavLink  className='link_nav contact_but'to='/contact' onClick={() => setIsOpen(false)}> <button>Contact</button></NavLink>  
+        {/* <Link  className='link_nav'to='/Query' onClick={() => setIsOpen(false)}><li>Query</li></Link>   */}
       </ul>
+    
     </div>
   )
 }
